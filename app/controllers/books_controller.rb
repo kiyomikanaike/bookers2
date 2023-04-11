80d4c417a5f58e0c2532005bf621527feebd1f4d
kiyomikanaike <kiyomi.kanaike@gmail.com>
2023-04-11 14:33:59 +0000

    Save

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,37 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
+    @book =Book.new
+    @books =Book.all
+    @user = current_user
   end
 
   def show
   end
 
   def edit
+    @books = Book.find(params[:id])
   end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
